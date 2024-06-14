e7bb6a3cd5a38d3fa8c02154856950b1244e7e60
SUNGYONG JANG <jsy@adain.kr>
2024-06-14 23:14:01 +0900

    add SIGINT process exit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import "dotenv/config";
-import { App } from "./app";
+import 'dotenv/config';
+import { App } from './app';
 
-process.on("uncaughtException", function (err) {
+process.on('uncaughtException', function (err) {
   console.error(err);
 });
 
-process.on("SIGINT", (err) => {
-  console.error("SIGINT", err);
+process.on('SIGINT', err => {
+  console.error('SIGINT', err);
+  process.exit();
 });
 
 const app = new App();
