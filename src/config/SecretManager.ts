6f0c2c818a450233f3d68f6cf949c4dd9e4c2c06
SUNGYONG JANG <jsy@adain.kr>
2025-03-12 11:50:02 +0900

    AWS Secrets Manager Client Configuration Improvement: Modify to Set Credential Options Conditionally

--- src/config/SecretManager.ts
+++ src/config/SecretManager.ts
@@ -5,13 +5,18 @@ export class SecretManager {
   private static awsSecretsManagerClient: SecretsManagerClient;
 
   static setupAWSSecretsManagerClient() {
-    SecretManager.awsSecretsManagerClient = new SecretsManagerClient({
+    let options: { region: string; credentials?: { accessKeyId: string; secretAccessKey: string } } = {
       region: AWS_REGION,
-      credentials: {
+    };
+
+    if (AWS_SECRET_KEY_ID && AWS_SECRET_ACCESS_KEY) {
+      options.credentials = {
         accessKeyId: AWS_SECRET_KEY_ID,
         secretAccessKey: AWS_SECRET_ACCESS_KEY,
-      },
-    });
+      };
+    }
+
+    SecretManager.awsSecretsManagerClient = new SecretsManagerClient(options);
   }
 
   static async getKey(id: string, key: string): Promise<string> {
